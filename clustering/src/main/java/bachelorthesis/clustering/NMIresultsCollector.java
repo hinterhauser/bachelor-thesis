49738e508cb73da6b3214aa8bac01c0b1d2573eb
Hermann Hinterhauser <a00946522@unet.univie.ac.at>
2018-08-10 21:53:39 +0200

    fixed preparation for visualisation

--- clustering/src/main/java/bachelorthesis/clustering/NMIresultsCollector.java
+++ clustering/src/main/java/bachelorthesis/clustering/NMIresultsCollector.java
@@ -1,6 +1,5 @@
 package bachelorthesis.clustering;
 
-import bachelorthesis.clustering.clustering.DBSCANer;
 import bachelorthesis.clustering.fileIO.FileIO;
 
 import java.io.FileWriter;
@@ -15,20 +14,12 @@ public class NMIresultsCollector {
     private static List<Double> DBSCANresults = new ArrayList<>();
     private static List<Double> hierarchicalResults = new ArrayList<>();
 
-    private static final String directory = "/results/performanceTests/results";
+    private static final String directory = "results/performanceTests/results";
 
     public static void main(String[] args) {
 
-        collectDataSerieOne();
-        writeResults(10);
-    }
-
-    private static void writeResults(int length) {
-
-        writeResultsToFile("firstSerieNear.txt", 10);
-        writeResultsToFile("firstSerieOver.txt", 10);
-        writeResultsToFile("firstSerieArbitrary.txt", 10);
-        writeResultsToFile("firstSerieMoons.txt", 10);
+        collectResultsOne();
+        collectResultsTwo();
     }
 
     private static void writeResultsToFile(String filename, int length) {
@@ -55,28 +46,73 @@ public class NMIresultsCollector {
         }
     }
 
-    private static void collectDataSerieOne() {
+    private static void collectResultsTwo() {
 
-        for (int i = 1; i <= 10; ++i) {
-
-            collectResults("/MDL", "mdl", 5, i, mdlResults);
-            collectResults("/Kmean", "kmean", 5, i, kmeansResults);
-            collectResults("/DBSCAN", "dbscan", 5, i, DBSCANresults);
-            collectResults("/hier", "hier", 5, i, hierarchicalResults);
+        for (int i = 5; i <= 50; i+=5) {
+            getNMIresults("near", i, 1);
         }
+        writeResultsToFile("secondSerieNear.txt", 10);
+        clearLists();
+        for (int i = 5; i <= 50; i+=5) {
+            getNMIresults("over", i, 1);
+        }
+        writeResultsToFile("secondSerieOver.txt", 10);
+        clearLists();
+        for (int i = 5; i <= 50; i+=5) {
+            getNMIresults("arbitrary", i, 1);
+        }
+        writeResultsToFile("secondSerieArbitrary.txt", 10);
+        clearLists();
+        for (int i = 5; i <= 50; i+=5) {
+            getNMIresults("moons", i, 1);
+        }
+        writeResultsToFile("secondSerieMoons.txt", 10);
+        clearLists();
     }
 
-    private static void collectResults(String dir, String algorithm, int dim, int factor, List<Double> resultList) {
+    private static void getNMIresults(String type, int dim, int factor) {
 
-        getNMIresult(dir, algorithm, "near", dim, factor, resultList);
-        getNMIresult(dir, algorithm, "over", dim, factor, resultList);
-        getNMIresult(dir, algorithm, "arbitrary", dim, factor, resultList);
-        getNMIresult(dir, algorithm, "moons", dim, factor, resultList);
+        getNMIresult("/MDL", "mdl", type, dim, factor, mdlResults);
+        getNMIresult("/Kmean", "kmean", type, dim, factor, kmeansResults);
+        getNMIresult("/DBSCAN", "dbscan", type, dim, factor, DBSCANresults);
+        getNMIresult("/hier", "hier", type, dim, factor, hierarchicalResults);
+    }
+
+    private static void clearLists() {
+
+        mdlResults.clear();
+        kmeansResults.clear();
+        DBSCANresults.clear();
+        hierarchicalResults.clear();
+    }
+
+    private static void collectResultsOne() {
+
+        for (int i = 1; i <= 10; ++i) {
+            getNMIresults("near", 5, i);
+        }
+        writeResultsToFile("firstSerieNear.txt", 10);
+        clearLists();
+        for (int i = 1; i <= 10; ++i) {
+            getNMIresults("over", 5, i);
+        }
+        writeResultsToFile("firstSerieOver.txt", 10);
+        clearLists();
+        for (int i = 1; i <= 10; ++i) {
+            getNMIresults("arbitrary", 5, i);
+        }
+        writeResultsToFile("firstSerieArbitrary.txt", 10);
+        clearLists();
+        for (int i = 1; i <= 10; ++i) {
+            getNMIresults("moons", 5, i);
+        }
+        writeResultsToFile("firstSerieMoons.txt", 10);
+        clearLists();
     }
 
     private static void getNMIresult(String dir, String algorithm, String type, int dim, int factor, List<Double> resultList) {
 
-        String fileName = directory + dir + "/compare_";
+        String fileName = directory + dir + "/results" + "/compare_";
         fileName += algorithm + "_" + type + "_dim" + dim;
         fileName += "size" + factor;
         fileName += "x_results.txt";
